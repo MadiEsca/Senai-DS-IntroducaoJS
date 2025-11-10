8b50488c09168237b281f9348d3530864022fd9a
botat <marcosvinny354@gmail.com>
2025-11-09 21:01:26 -0300

    add: Atividade

---
+++ script.js
@@ -0,0 +1,25 @@
+// atividade 01
+ let nome=prompt("Qual o seu nome?")
+ let continuar = confirm("Boa vindas!")
+ if(continuar){
+    alert("Bem vindo " + nome + "!")
+    // console.log("Bem vindo " + nome + "!" )
+ }else{
+    alert("Usuário cancelou a mensagem")
+    // console.log("Usuário cancelou a mensagem")
+ }
+
+
+// atividade 02
+// function saudacao(nome){
+//     console.log("Olá " + nome)
+// }
+// saudacao("Maria")
+// saudacao("Jose")
+// saudacao("Roberto")
+
+//atividade 3
+// (function (){
+//     console.log("Função executada")
+// })
+// ()
